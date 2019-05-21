96281a6dda60268f610bdfd3183c7e9c3f38e8df
杨浩艺 <yanghy@mapbar.com>
2019-05-21 17:49:57 +0800

    [CS-新增测试代码]:[新增测试]

--- app/src/main/java/com/yanghaoyi/yretrofit/MainActivity.java
+++ app/src/main/java/com/yanghaoyi/yretrofit/MainActivity.java
@@ -3,6 +3,8 @@ package com.yanghaoyi.yretrofit;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.yanghaoyi.net.client.ApiClient;
+import com.yanghaoyi.net.client.OnGetDataListener;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,16 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        new NearbySearchModel().requestChargeStationList(new OnGetDataListener<NetData>() {
+            @Override
+            public void success(NetData response) {
+
+            }
+
+            @Override
+            public void fail(NetData response, String msg) {
+
+            }
+        });
     }
 }
